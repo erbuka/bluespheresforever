24cf511b3f3527436d6e4f8ba1f2fa15b52161fc
erbuka <erbuka@hotmail.com>
2018-10-06 12:25:49 +0200

    Removing docs

--- docs/js/main.js
+++
@@ -1,7 +0,0 @@
-(function() {
-	$(() => {
-		$("div.bsf-menu button").on("click", function() {
-			$(`section#${$(this).attr("bsf-section")}`)[0].scrollIntoView({ behavior : "smooth", block : "start" });
-		});
-	});
-})();
